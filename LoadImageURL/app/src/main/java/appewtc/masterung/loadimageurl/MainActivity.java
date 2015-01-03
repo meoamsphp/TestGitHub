fe751e99016717f31c5be906e5968c3b59b2e481
masterUNG <phrombutr@gmail.com>
2015-01-03 23:07:57 +0700

    finish 1
    สำเร็จ ครั้งแรก

--- LoadImageURL/app/src/main/java/appewtc/masterung/loadimageurl/MainActivity.java
+++ LoadImageURL/app/src/main/java/appewtc/masterung/loadimageurl/MainActivity.java
@@ -1,18 +1,106 @@
 package appewtc.masterung.loadimageurl;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    //Explicit
+    private ImageView imvShow;
+
+    public static final String strImageURL = "http://swiftcodingthai.com/test1/animal/cow.png";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        //Bind Widget
+        imvShow = (ImageView) findViewById(R.id.imageView);
+
+        GetXMLTask objGetXMLTask = new GetXMLTask();
+        objGetXMLTask.execute(new String[] {strImageURL});
+
+    }   // onCreate
+
+    private class GetXMLTask extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+
+            Bitmap objBitmap = null;
+            for (String strUrl : params) {
+                objBitmap = downloadImage(strUrl);
+            }   // for
+
+            return objBitmap;
+        }   // doInBackground
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            imvShow.setImageBitmap(bitmap);
+        }   // onPostExecute
+
+        private Bitmap downloadImage(String strUrl) {
+
+            Bitmap objBitmap = null;
+            InputStream objInputStream = null;
+            BitmapFactory.Options objBitmapFactory = new BitmapFactory.Options();
+            objBitmapFactory.inSampleSize = 1;
+
+            try {
+
+                objInputStream = getHttpConnection(strUrl);
+                objBitmap = BitmapFactory.decodeStream(objInputStream, null, objBitmapFactory);
+                objInputStream.close();
+
+            } catch (Exception e) {
+                Log.d("test", "downloadImage ==> " + e.toString());
+            }
+
+            return objBitmap;
+        }   // downloadImage
+
+        private InputStream getHttpConnection(String strUrl) throws IOException {
+
+            InputStream objInputStream = null;
+            URL objURL = new URL(strUrl);
+            URLConnection objURLConnection = objURL.openConnection();
+
+            try {
+
+                HttpURLConnection objHttpURLConnection = (HttpURLConnection) objURLConnection;
+                objHttpURLConnection.setRequestMethod("GET");
+                objHttpURLConnection.connect();
+
+                if (objHttpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    objInputStream = objHttpURLConnection.getInputStream();
+                }   // if
+
+            } catch (Exception e) {
+                Log.d("test", "getHttpConnection ==> " + e.toString());
+            }
+
+            return objInputStream;
+        }   // getHttpConnection
+
+
+    }   // GetXMLTask Clsaa
 
 
     @Override
@@ -36,4 +124,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+}   // Main Class
