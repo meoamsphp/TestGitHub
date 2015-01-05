14302cb9f5bc0c6c0689f5b19ee9684e473e2932
masterUNG <phrombutr@gmail.com>
2015-01-05 19:18:19 +0700

    Syn JSON
    Syn JSON สำหร้บ Table user

--- OSPTraining/app/src/main/java/appewtc/masterung/osptraining/MainActivity.java
+++ OSPTraining/app/src/main/java/appewtc/masterung/osptraining/MainActivity.java
@@ -1,9 +1,24 @@
 package appewtc.masterung.osptraining;
 
+import android.os.Build;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -26,9 +41,85 @@ public class MainActivity extends ActionBarActivity {
         //Tester
         testerAddData();
 
+        //Syn JSON to SQLite
+        synJSONtoSQLite();
+
 
 
     }   // onCreate
+
+    private void synJSONtoSQLite() {
+
+        //setUP Policy
+        if (Build.VERSION.SDK_INT > 9) {
+
+            StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(myPolicy);
+
+        }   // if
+
+
+        InputStream objInputStream = null;
+        String strJSON = "";
+
+        //Create InputStream
+        try {
+
+            HttpClient objHttpClient = new DefaultHttpClient();
+            HttpPost objHttpPost = new HttpPost("http://swiftcodingthai.com/osp/php_get_data_master.php");
+            HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+            HttpEntity objHttpEntity = objHttpResponse.getEntity();
+            objInputStream = objHttpEntity.getContent();
+
+        } catch (Exception e) {
+            Log.d("osp", "Error from InputStream ==>  " + e.toString());
+        }   // Create InputStream
+
+
+        //Create strJSON
+        try {
+
+            BufferedReader objBufferedRader = new BufferedReader(new InputStreamReader(objInputStream, "UTF-8"));
+            StringBuilder objStringBuilder = new StringBuilder();
+            String strLine = null;
+
+            while ((strLine = objBufferedRader.readLine()) != null) {
+
+                objStringBuilder.append(strLine);
+
+            }   // while
+
+            objInputStream.close();
+            strJSON = objStringBuilder.toString();
+
+        } catch (Exception e) {
+            Log.d("osp", "Error from strJSON ==> " + e.toString());
+        }   //Create strJSON
+
+
+        //Up New Data to SQLite
+        try {
+
+            final JSONArray objJSONArray = new JSONArray(strJSON);
+
+            for (int i = 0; i < objJSONArray.length(); i++) {
+
+                JSONObject objJSONObject = objJSONArray.getJSONObject(i);
+                String strUser = objJSONObject.getString("User");
+                String strPassword = objJSONObject.getString("Password");
+                String strName = objJSONObject.getString("Name");
+
+                long addUser = objUserTABLE.updateUser(MainActivity.this, strUser, strPassword, strName);
+
+            }   // for
+
+        } catch (Exception e) {
+            Log.d("osp", "Error from Up Data ==> " + e.toString());
+        }   // up Data
+
+
+
+    }   // synJSONtoSQLite
 
     private void testerAddData() {
 
