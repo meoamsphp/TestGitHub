1a4148edfd07bab9184cc735cfd9f28c4189e714
masterUNG <phrombutr@gmail.com>
2015-01-05 22:49:14 +0700

    Finish
    เสร็จละ

--- DownloadImageURL/app/src/main/java/appewtc/masterung/downloadimageurl/MainActivity.java
+++ DownloadImageURL/app/src/main/java/appewtc/masterung/downloadimageurl/MainActivity.java
@@ -1,39 +1,84 @@
 package appewtc.masterung.downloadimageurl;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.URL;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    //Explicit
+    private String strMyImageURL = "https://farm6.staticflickr.com/5546/10306091065_1dd4531d50_o.jpg";
+    private ImageView imvShowImage;
+    private ProgressDialog objProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        //Bind Widget
+        imvShowImage = (ImageView) findViewById(R.id.imageView);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    }   // onCreate
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void clickShowImage(View view) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //Execute DownloadImage AsyncTask
+        new DownloadImage().execute(strMyImageURL);
 
-        return super.onOptionsItemSelected(item);
-    }
-}
+    }   // clickShowImage
+
+    //Create DownloadImage AsyncTask Class
+    private class DownloadImage extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //Create Progressdialog
+            objProgressDialog = new ProgressDialog(MainActivity.this);
+            objProgressDialog.setTitle("Download Image");
+            objProgressDialog.setMessage("Load Image Wait Please");
+            objProgressDialog.setIndeterminate(false);
+            objProgressDialog.show();
+        }   // onPreExecute
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+
+            String strImageURL = params[0];
+            Bitmap objBitmap = null;
+
+            try {
+                //Download Image from URL
+                //InputStream objInputStream = new java.net.URL(strImageURL).openStream();
+                InputStream objInputStream = new URL(strImageURL).openStream();
+                //Decode Bitmap
+                objBitmap = BitmapFactory.decodeStream(objInputStream);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }   // try
+
+            return objBitmap;
+        }   // doInBackground
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            // super.onPostExecute(bitmap);
+            //Set bitmap to ImageView
+            imvShowImage.setImageBitmap(bitmap);
+            objProgressDialog.dismiss();
+        }   // onPOstExecute
+
+    }   // DownloadImage Class
+
+}   // Main Class
