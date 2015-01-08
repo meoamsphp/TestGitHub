1ba0e3ec80f77917e805c244740e2d7b85ed3115
masterUNG <phrombutr@gmail.com>
2015-01-08 10:29:43 +0700

    Start Maps API2
    Start Maps OK

--- OSPTraining/app/src/main/java/appewtc/masterung/osptraining/MyMapActivity.java
+++ OSPTraining/app/src/main/java/appewtc/masterung/osptraining/MyMapActivity.java
@@ -11,7 +11,7 @@ public class MyMapActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_map);
+        setContentView(R.layout.map_layout);
     }
 
 
